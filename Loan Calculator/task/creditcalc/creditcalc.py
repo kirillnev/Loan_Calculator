544bf87dc2cede0fbb1ddcf1f19b31e66b3011bc
Kirill Nevinskii <mail@kirillnev.ru>
2023-02-16 16:31:22 +0600

    Stage_4_4

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,4 +1,6 @@
+import argparse
 import math
+
 
 def months_to_str(n):
     y = n // 12
@@ -6,48 +8,74 @@ def months_to_str(n):
     res = 'It will take '
     if y > 0:
         res += f'{y} year{"s" if y > 1 else ""}'
-    res += ' and ' if m > 0 else ''
-    res += f'{m} month{"s" if m > 1 else ""}'
+    res += ' and ' if m > 0 and y > 0 else ''
+    if m > 0:
+        res += f'{m} month{"s" if m > 1 else ""}'
     res += ' to repay this loan!'
     return res
 
+
+def calculate_annuity_n(args):
+    i = args['interest'] / (12 * 100)
+    months_to_repay = math.ceil(math.log(args['payment'] / (args['payment'] - i * args['principal']), i + 1))
+    print(months_to_str(months_to_repay))
+    print(f"Overpayment = {args['payment'] * months_to_repay - args['principal']}")
+
+
+def calculate_annuity_a(args):
+    i = args['interest'] / (12 * 100)
+    payment = math.ceil(args['principal'] * i * math.pow(1 + i, args['periods']) / (math.pow(1 + i, args['periods']) - 1))
+    print(f"Your annuity payment = {payment}!")
+    print(f"Overpayment = {payment * args['periods'] - args['principal']}")
+
+
+def calculate_annuity_p(args):
+    i = args['interest'] / (12 * 100)
+    loan_principal = math.floor(args['payment'] / (i * math.pow(1 + i, args['periods']) / (math.pow(1 + i, args['periods']) - 1)))
+    print(f'Your loan principal = {loan_principal}!')
+
+
+def calculate_diff(args):
+    s = 0
+    i = args['interest'] / (12 * 100)
+    for m in range(1, args['periods'] + 1):
+        dm = math.ceil(args['principal'] / args['periods'] + i * args['principal'] * (1 - (m - 1) / args['periods']))
+        s += dm
+        print(f"Month {m}: payment is {dm}")
+    print(f"Overpayment = {s - args['principal']}")
+
+
 def main():
-    print('What do you want to calculate?')
-    print('type "n" - for number of monthly payments,')
-    print('type "a" - for annuity monthly payment amount,')
-    print('type "p" - for the monthly payment:')
-    cmd = input()
-    if cmd == 'n':
-        print('Enter the loan principal:')
-        loan_principal = float(input())
-        print('Enter the monthly payment:')
-        payment = float(input())
-        print('Enter the loan interest:')
-        loan_interest = float(input())
-        i = loan_interest / (12 * 100)
-        months_to_repay = math.ceil(math.log(payment / (payment - i * loan_principal), i + 1))
-        print(months_to_str(months_to_repay))
-    elif cmd == 'a':
-        print('Enter the loan principal:')
-        loan_principal = float(input())
-        print('Enter the number of periods:')
-        months = int(input())
-        print('Enter the loan interest:')
-        loan_interest = float(input())
-        i = loan_interest / (12 * 100)
-        payment = math.ceil(loan_principal * i * math.pow(1 + i, months) / (math.pow(1 + i, months) - 1))
-        # last_payment = loan_principal - (months - 1) * payment
-        print(f'Your monthly payment = {payment}!')
+    invalid_params_msg = 'Incorrect parameters'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--principal", type=int, default=0)
+    parser.add_argument("--payment", type=int, default=0)
+    parser.add_argument("--periods", type=int, default=0)
+    parser.add_argument("--interest", type=float, default=0)
+    parser.add_argument("--type", type=str, default='')
+    args = vars(parser.parse_args())
+    if (args.get('type') == 'annuity'
+            and args.get('principal') > 0
+            and args.get('payment') > 0
+            and args.get('interest') > 0):
+        calculate_annuity_n(args)
+    elif (args.get('type') == 'annuity'
+            and args.get('principal') > 0
+            and args.get('periods') > 0
+            and args.get('interest') > 0):
+        calculate_annuity_a(args)
+    elif (args.get('type') == 'annuity'
+            and args.get('payment') > 0
+            and args.get('periods') > 0
+            and args.get('interest') > 0):
+        calculate_annuity_p(args)
+    elif (args.get('type') == 'diff'
+            and args.get('principal') > 0
+            and args.get('periods') > 0
+            and args.get('interest') > 0):
+        calculate_diff(args)
     else:
-        print('Enter the annuity payment:')
-        annuity_payment = float(input())
-        print('Enter the number of periods:')
-        months = int(input())
-        print('Enter the loan interest:')
-        loan_interest = float(input())
-        i = loan_interest / (12 * 100)
-        loan_principal = math.floor(annuity_payment / (i * math.pow(1 + i, months) / (math.pow(1 + i, months) - 1)))
-        print(f'Your loan principal = {loan_principal}!')
+        print(invalid_params_msg)
 
 
 if __name__ == '__main__':
